68b70dff5b55e00bf3762f867fea515994e1dd1a
Jose Ortiz <jose.oc@gmail.com>
2026-05-04 11:57:29 +0200

    Linkedin profile URL

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   social: {
     // Add your social links here. If you leave them empty or remove them, the icons will disappear.
-    linkedin: "https://linkedin.com/in/jose-oc",
+    linkedin: "https://www.linkedin.com/in/joseoc/",
     twitter: "https://twitter.com/_jose_oc",
     github: "https://github.com/jose-oc",
   }
