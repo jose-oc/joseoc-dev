3253e28d91ad8cd908da0cd0fca325d59e047d71
Jose Ortiz <jose.oc@gmail.com>
2026-05-04 11:54:30 +0200

    Astro feature: Mermaid diagrams

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,27 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import rehypeMermaid from 'rehype-mermaid';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
+  },
+
+  markdown: {
+    syntaxHighlight: {
+      type: 'shiki',
+      excludeLangs: ['mermaid'],
+    },
+    rehypePlugins: [
+      [rehypeMermaid, { 
+        strategy: 'inline-svg',
+        mermaidConfig: {
+          theme: 'neutral'
+        }
+      }]
+    ]
   },
 
   integrations: [mdx(), sitemap()],
